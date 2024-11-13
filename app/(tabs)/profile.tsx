f38a5f5b975d3e5718f7bfab1648358a74082515
thinhtethtetzaw <thinhtethtetzaw778@gmail.com>
2024-11-13 15:26:03 +0700

    profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Button } from "react-native";
+import { View, Text, Button } from "react-native";
 import { router } from "expo-router";
 import { useAuth } from "./../context/AuthContext";
 
@@ -11,47 +11,22 @@ export default function ProfileScreen() {
   };
 
   return (
-    <View style={styles.container}>
+    <View className="flex-1 p-5 bg-white">
       {user ? (
-        <View style={styles.profileInfo}>
-          <Text style={styles.title}>Profile</Text>
-          <Text style={styles.info}>Name: {user.name}</Text>
-          <Text style={styles.info}>Email: {user.email}</Text>
-          <Text style={styles.info}>
+        <View className="flex-1 items-center justify-center">
+          <Text className="text-2xl font-bold mb-5">Profile</Text>
+          <Text className="text-base mb-2.5">Name: {user.name}</Text>
+          <Text className="text-base mb-2.5">Email: {user.email}</Text>
+          <Text className="text-base mb-2.5">
             Joined: {new Date(user.createdAt).toLocaleDateString()}
           </Text>
         </View>
       ) : (
         <Text>Loading profile...</Text>
       )}
-      <View style={styles.logoutButton}>
-        <Button title="Logout" onPress={handleLogout} color="red" />
+      <View className="mb-5">
+        <Button title="Logout" onPress={handleLogout} color="#fb4b54" />
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: "white",
-  },
-  profileInfo: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  info: {
-    fontSize: 16,
-    marginBottom: 10,
-  },
-  logoutButton: {
-    marginBottom: 20,
-  },
-});
